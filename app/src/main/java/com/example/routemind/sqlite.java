7e1a60ca111ee426b2b200ef8de0e19f5ca80d91
Kenrawr <20102188@usc.edu.ph>
2026-04-15 01:05:49 +0800

    Added : SQlite & finished Nav

--- app/src/main/java/com/example/routemind/sqlite.java
+++ app/src/main/java/com/example/routemind/sqlite.java
@@ -8,14 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class UserProfile extends AppCompatActivity {
+public class sqlite extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_user_profile);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottom_navigation), (v, insets) -> {
+        setContentView(R.layout.activity_sqlite);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
