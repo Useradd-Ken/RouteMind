ce5892562e5062b8ab1cd3e6eed16a07ba97fe26
Kenrawr <20102188@usc.edu.ph>
2026-04-21 21:37:01 +0800

    Functional Nav Bar, switches through pages.

--- app/src/main/java/com/example/routemind/TripHistory.java
+++ app/src/main/java/com/example/routemind/TripHistory.java
@@ -1,5 +1,6 @@
 package com.example.routemind;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,6 +9,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class TripHistory extends AppCompatActivity {
 
     @Override
@@ -15,10 +18,36 @@ public class TripHistory extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_trip_history);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottom_navigation), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.nav_trip_history);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_home) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                overridePendingTransition(0, 0);
+                return true;
+            } else if (id == R.id.nav_activities) {
+                startActivity(new Intent(getApplicationContext(), sqlite.class));
+                overridePendingTransition(0, 0);
+                return true;
+            } else if (id == R.id.nav_maps) {
+                return true;
+            } else if (id == R.id.nav_trip_history) {
+                return true;
+            } else if (id == R.id.nav_user_profile) {
+                startActivity(new Intent(getApplicationContext(), UserProfile.class));
+                overridePendingTransition(0, 0);
+                return true;
+            }
+            return false;
         });
     }
 }
--- app/src/main/java/com/example/routemind/UserProfile.java
+++ app/src/main/java/com/example/routemind/UserProfile.java
@@ -1,5 +1,6 @@
 package com.example.routemind;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,6 +9,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class UserProfile extends AppCompatActivity {
 
     @Override
@@ -15,10 +18,37 @@ public class UserProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_profile);
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottom_navigation), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.nav_user_profile);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_home) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                overridePendingTransition(2, 3);
+                return true;
+            } else if (id == R.id.nav_activities) {
+                startActivity(new Intent(getApplicationContext(), sqlite.class));
+                overridePendingTransition(0, 0);
+                return true;
+            } else if (id == R.id.nav_maps) {
+                // If you have a Maps activity, add it here.
+                return true;
+            } else if (id == R.id.nav_trip_history) {
+                startActivity(new Intent(getApplicationContext(), TripHistory.class));
+                overridePendingTransition(0, 0);
+                return true;
+            } else if (id == R.id.nav_user_profile) {
+                return true;
+            }
+            return false;
         });
     }
 }
